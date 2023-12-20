5e2625001ed2d5b65cfc6103819c75735b4f53fe
Vishnu Mano <vishnu.mano@gmail.com>
2023-12-20 07:21:56 -0500

    final

---
+++ tools.py
@@ -0,0 +1,21 @@
+# file handling: navigate, rename, move, copy, remove
+
+import os
+
+# print(os.getcwd()) # pwd
+os.chdir("/Users/vishnumano/Desktop") # cd
+# print(os.listdir()) # ls
+
+# Rename
+counter = 1
+for file in os.listdir():
+    if file[0] == ".":
+        continue
+    name, ext = os.path.splitext(file)
+    print(name)
+    print(ext)
+    splitted = name.split(" ")
+    splitted = [s.strip() for s in splitted]
+    new_name = f"{str(counter).zfill(2)}->{splitted[1]}_{splitted[3]}{splitted[4]}->{splitted[0]}{ext}"
+    os.rename(file, new_name)
+    counter += 1
